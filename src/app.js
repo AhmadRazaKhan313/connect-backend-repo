041a6a5e524d683b070b6bd7e038b65be5af5367
AhmadRazaKhan313 <ahmadrazakhan300@gmail.com>
2026-07-01 07:29:41 +0500

    chore: remove debug routes + duplicate subdomain middleware

--- src/app.js
+++ src/app.js
@@ -10,13 +10,10 @@ const routes = require("./routes/v1");
 const { errorConverter, errorHandler } = require("./middlewares/error");
 const cron = require("node-cron-tz");
 const ApiError = require("./utils/ApiError");
-const { sendTemplateForExpiry } = require("./services/email.service");
 const summaryController = require("./modules/summary/summary.controller");
-const { EntryModel } = require("./models");
 const { Mutex } = require("async-mutex");
 const mutex = new Mutex();
 const subdomainMiddleware = require("./middlewares/subdomain");
-const { OrganizationModel } = require("./models");
 
 
 
@@ -46,42 +43,11 @@ app.use(mongoSanitize());
 app.use(passport.initialize());
 passport.use("jwt", jwtStrategy);
 
-
-app.use(async (req, res, next) => {
-    const host = req.headers.host;
-    // karachi.local:4000 → parts = ["karachi", "local:4000"]
-    const parts = host.split(".");
-    const subdomain = parts[0];
-
-    const ignored = ["localhost", "local", "www", "127", "api"];
-
-    if (ignored.includes(subdomain) || parts.length < 2) {
-        return next();
-    }
-
-    try {
-        const org = await OrganizationModel
-            .findOne({ subdomain });
-        if (org) {
-            req.organizationId = org._id;
-        }
-    } catch (err) {
-        console.log('Subdomain error:', err);
-    }
-
-    next();
-});
-
 // v1 api routes
 app.use("/api/v1", routes);
 app.use("/api/v1/test", async (req, res) => {
   res.send(`Server is successfully up`);
 });
-
-app.use('/whatsapp', async (req, res) => {
-  const response = await sendTemplateForExpiry("923062244907", 'Haseeb', '1', '123', 'Tuesday')
-  res.send(response)
-})
 
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
